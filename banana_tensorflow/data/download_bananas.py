b4e8a754f1c3097fc2308fce3ef10da3de7d1202
adargut <adar276@gmail.com>
2019-10-10 01:15:42 +0300

    Minor changes

---
+++ banana_tensorflow/data/download_bananas.py
@@ -0,0 +1,15 @@
+# Import google images download
+from google_images_download import google_images_download
+
+# Construct a response object
+response = google_images_download.googleimagesdownload()
+
+# Search query for google images
+args = {"keywords": "rotten banana",
+        "limit": 80,  # TODO maybe a higher limit is needed for an efficient network?
+        "print_urls": True,
+        "size": "medium"}
+
+# Download all images and print their absolute values
+paths = response.download(args)
+print(paths)

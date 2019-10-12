6fbe14bc12c73df216b61818787a7d63b5f03ee7
adargut <adar276@gmail.com>
2019-10-13 01:27:10 +0300

    Added ImageClassifier class

---
+++ banana_app/app/src/main/java/com/example/perfectbanana/ImageClassifier.java
@@ -0,0 +1,197 @@
+package com.example.perfectbanana;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.os.SystemClock;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import org.tensorflow.lite.Interpreter;
+
+/** Classify bananas using TF Lite */
+public class ImageClassifier {
+
+    /** Tag for {@link} log */
+    private static final String TAG = "PerfectBanana";
+
+    /** Paths for labels and model */
+    private static String modelPath = "graph.tflite";
+    private static String labelPath = "labels.txt";
+
+    /** Show probabilities for 3 results */
+    private static final int RESULTS_TO_SHOW = 3;
+
+    /** Input dimensions below */
+    private static final int DIM_BATCH_SIZE = 1;
+    private static final int DIM_PIXEL_SIZE = 3;
+    static final int DIM_IMG_SIZE_X = 224;
+    static final int DIM_IMG_SIZE_Y = 224;
+    private static final int IMAGE_MEAN = 128;
+    private static final float IMAGE_STD = 128.0f;
+
+    /** Allocate buffer for storing images */
+    private int[] intValues = new int[DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y];
+
+    /** Instance of model interpreter */
+    private Interpreter tflite;
+
+    /** Store labels */
+    private List<String> labelList;
+
+    /** Store image data to feed into tensorflow as input */
+    private ByteBuffer imgData = null;
+
+    /** Store inference results, feed into tensorflow as output */
+    private float[][] labelProbArray = null;
+
+    /** Multi-stage low pass filter **/
+    private float[][] filterLabelProbArray = null;
+    private static final int FILTER_STAGES = 3;
+    private static final float FILTER_FACTOR = 0.4f;
+
+    private PriorityQueue<Map.Entry<String, Float>> sortedLabels =
+            new PriorityQueue<>(
+                    RESULTS_TO_SHOW,
+                    new Comparator<Map.Entry<String, Float>>() {
+                        @Override
+                        public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
+                            return (o1.getValue()).compareTo(o2.getValue());
+                        }
+                    });
+
+    /** Initialize an {@code ImageClassifier} */
+    ImageClassifier(Activity activity) throws IOException {
+        tflite = new Interpreter(loadModelFile(activity));
+        labelList = loadLabelList(activity);
+        imgData = ByteBuffer.allocateDirect(
+                        4 * DIM_BATCH_SIZE * DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y * DIM_PIXEL_SIZE);
+        imgData.order(ByteOrder.nativeOrder());
+        labelProbArray = new float[1][labelList.size()];
+        filterLabelProbArray = new float[FILTER_STAGES][labelList.size()];
+        Log.d(TAG, "Created a TF Lite Image Classifier for bananas");
+    }
+
+    /** Classifies a frame */
+    String classifyFrame(Bitmap bitmap) {
+        if (tflite == null) {
+            Log.e(TAG, "Image classifier not initialized");
+            return "Uninitialized Classifier";
+        }
+        convertBitmapToByteBuffer(bitmap);
+        long startTime = SystemClock.uptimeMillis();
+        tflite.run(imgData, labelProbArray);
+        long endTime = SystemClock.uptimeMillis();
+        Log.d(TAG, "Timecost to run model inference: " + Long.toString(endTime - startTime));
+
+        // Smooth results
+        applyFilter();
+
+        // Print results
+        String textToShow = printTopKLabels();
+        textToShow = Long.toString(endTime - startTime) + "ms" + textToShow;
+        return textToShow;
+    }
+
+    void applyFilter() {
+        int num_labels = labelList.size();
+
+        // Low pass filter `labelProbArray` into the first stage of the filter.
+        for(int j=0; j<num_labels; ++j){
+            filterLabelProbArray[0][j] += FILTER_FACTOR*(labelProbArray[0][j] -
+                    filterLabelProbArray[0][j]);
+        }
+        // Low pass filter each stage into the next.
+        for (int i=1; i<FILTER_STAGES; ++i){
+            for(int j=0; j<num_labels; ++j){
+                filterLabelProbArray[i][j] += FILTER_FACTOR*(
+                        filterLabelProbArray[i-1][j] -
+                                filterLabelProbArray[i][j]);
+            }
+        }
+        // Copy the last stage filter output back to `labelProbArray`.
+        for(int j=0; j<num_labels; ++j){
+            labelProbArray[0][j] = filterLabelProbArray[FILTER_STAGES-1][j];
+        }
+    }
+
+    /** Free tflite resources */
+    public void close() {
+        tflite.close();
+        tflite = null;
+    }
+
+    /** Reads label list from Assets. */
+    private List<String> loadLabelList(Activity activity) throws IOException {
+        List<String> labelList = new ArrayList<String>();
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(activity.getAssets().open(labelPath)));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            labelList.add(line);
+        }
+        reader.close();
+        return labelList;
+    }
+
+    /** Reads tensorflow model from Assets. */
+    private MappedByteBuffer loadModelFile(Activity activity) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(modelPath);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    /** Writes image data as {@code ByteBuffer} */
+    private void convertBitmapToByteBuffer(Bitmap bitmap) {
+        if (imgData == null) {
+            return;
+        }
+        imgData.rewind();
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        int pixel = 0;
+        long startTime = SystemClock.uptimeMillis();
+        for (int i = 0; i < DIM_IMG_SIZE_X; ++i) {
+            for (int j = 0; j < DIM_IMG_SIZE_Y; ++j) {
+                final int val = intValues[pixel++];
+                imgData.putFloat((((val >> 16) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
+                imgData.putFloat((((val >> 8) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
+                imgData.putFloat((((val) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
+            }
+        }
+        long endTime = SystemClock.uptimeMillis();
+        Log.d(TAG, "Timecost to put values into ByteBuffer: " + Long.toString(endTime - startTime));
+    }
+
+    /** Prints top 3 labels to be shown in UI as result */
+    private String printTopKLabels() {
+        for (int i = 0; i < labelList.size(); ++i) {
+            sortedLabels.add(
+                    new AbstractMap.SimpleEntry<>(labelList.get(i), labelProbArray[0][i]));
+            if (sortedLabels.size() > RESULTS_TO_SHOW) {
+                sortedLabels.poll();
+            }
+        }
+        String textToShow = "";
+        final int size = sortedLabels.size();
+        for (int i = 0; i < size; ++i) {
+            Map.Entry<String, Float> label = sortedLabels.poll();
+            textToShow = String.format("\n%s: %4.2f",label.getKey(),label.getValue()) + textToShow;
+        }
+        return textToShow;
+    }
+}
